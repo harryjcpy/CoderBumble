605d5c8a4af11a866336f3af91bf6960d41c64de
harryjcpy <harsheit.mishra@gmail.com>
2024-10-13 12:30:51 +0530

    Explore get, post, and delete

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ app.listen(7777, () => {
     console.log('Server Chalu ho chuka hai');
 });
 
+app.get("/user", (req, res) => {
+    res.send("Hello Bhai, Kya chahiye?");
+})
+app.post("/user", (req, res) => {
+    console.log('Post krne jaa rhe hain!');
+    res.send("Kya bhejna hai?");
+});
+app.delete("/user", (req, res) => {
+    res.send("Delete krne jaa rhe hain!");
+});
+
 app.use("/test", (req, res) => {
     res.send('Test me aapka swagat hai');
 });
